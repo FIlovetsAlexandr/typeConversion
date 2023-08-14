9aa00dd9e0ade2310034472298d0834556fc8a48
FIlovetsAlexandr <sohan.boxan@gmail.com>
2023-08-14 15:28:42 +0300

    project created

---
+++ HW12/HW12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW12
+//
+//  Created by MacBookPro15 on 14.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
