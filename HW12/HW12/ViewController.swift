4d00be25f1b3a731bca6f55850d3a64b9a5fe741
FIlovetsAlexandr <sohan.boxan@gmail.com>
2023-08-14 15:31:30 +0300

    add playground with project

--- HW12/HW12/ViewController.swift
+++ HW12/HW12/ViewController.swift
@@ -1,13 +1,7 @@
-//
-//  ViewController.swift
-//  HW12
-//
-//  Created by MacBookPro15 on 14.08.23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
